4f21af51a20ce43b776114ca12573e9e2c56e19b
Xander Smeets <xander@smeets.re>
2023-08-06 15:55:09 +0200

    Export ROCKET_PORT

--- entrypoint.sh
+++ entrypoint.sh
@@ -8,5 +8,8 @@ echo ":/home/container$ ${MODIFIED_STARTUP}"
 # Create data directory, if it does not exist
 mkdir -p /home/container/data
 
+# Make the server run on the allocated port
+export ROCKET_PORT=$SERVER_PORT
+
 # Run the Server
 ${MODIFIED_STARTUP}
